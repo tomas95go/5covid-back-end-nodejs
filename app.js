5ad0940db5222dc657f245796374acfaf2705560
tomas95go <tomas95go@gmail.com>
2020-03-24 16:56:49 -0300

    algunas cosas

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-let router = express.Router();
-getDuenios = require('./routes/index');
+const bodyParser = require('body-parser');
+let getDatos = require('./routes/index');
 
-app.use('/', getDuenios);
+app.use(bodyParser.json());
 
-app.use(`/user/:id`, getDuenios);
+app.use('/', getDatos);
+
+app.use('/listado_negocios', getDatos);
+
+app.use('/listado_negocios/:rubro', getDatos);
+
+app.use('/test', getDatos);
+
+app.use('/register_cliente', getDatos);
+
+app.use('login', getDatos);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
