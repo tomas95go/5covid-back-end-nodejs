1ee5510c455059cdd2eced979e7b0f26e0b4de9b
tomas95go <tomas95go@gmail.com>
2020-03-24 11:12:43 -0300

    adding db creds

--- config/db.js
+++ config/db.js
@@ -1,12 +1,11 @@
-/*let knex = require('knex')({
+let knex = require('knex')({
   client: 'mysql',
   connection: {
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'root',
-    database: '5covid',
+    host: 'us-cdbr-iron-east-01.cleardb.net',
+    user: 'be50d8aa2153f8',
+    password: '0f15e12e',
+    database: 'heroku_e5bb1b27e6461d4',
   },
 });
 
 module.exports = knex;
-*/
