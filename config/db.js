b5167c61735a641c835772d0cc04f3f7e3b53827
tomas95go <tomas95go@gmail.com>
2020-04-02 11:13:17 -0300

    Agregando la DB remota

--- config/db.js
+++ config/db.js
@@ -1,18 +1,20 @@
 let knex = require('knex')({
   client: 'mysql',
+  /*
+  LOCAL CLIENT
   connection: {
     host: 'localhost',
     user: 'root',
     password: 'root',
     database: '5covid',
-  },
-  /*REMOTE CONNECTION
+  },*/
+  //REMOTE CONNECTION
   connection: {
     host: 'us-cdbr-iron-east-01.cleardb.net',
     user: 'be50d8aa2153f8',
     password: '0f15e12e',
     database: 'heroku_e5bb1b27e6461d4',
-  },*/
+  },
 });
 
 module.exports = knex;
