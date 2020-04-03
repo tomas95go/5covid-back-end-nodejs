5be6d59301087b6a458889c68d7abcd83ed1e56b
tomas95go <tomas95go@gmail.com>
2020-04-03 10:44:33 -0300

    Agregando comentarios a la conexion de la dB

--- config/db.js
+++ config/db.js
@@ -1,14 +1,14 @@
 let knex = require('knex')({
   client: 'mysql',
   /*
-  LOCAL CLIENT
+  //Conexión local
   connection: {
     host: 'localhost',
     user: 'root',
     password: 'root',
     database: '5covid',
   },*/
-  //REMOTE CONNECTION
+  //Conexión remota.
   connection: {
     host: 'us-cdbr-iron-east-01.cleardb.net',
     user: 'be50d8aa2153f8',
