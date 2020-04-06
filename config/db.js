b2640523c350b3ced960e7f3e451bd8c7eb5f497
tomas95go <tomas95go@gmail.com>
2020-04-06 00:44:06 -0300

    probando registrar usuarios + clientes

--- config/db.js
+++ config/db.js
@@ -2,19 +2,19 @@ let knex = require('knex')({
   client: 'mysql',
 
   //Conexión local
-  connection: {
+  /*connection: {
     host: 'localhost',
     user: 'root',
     password: 'root',
     database: '5covid',
-  },
+  }*/,
   //Conexión remota. ClearDB de Heroku.
-  /*connection: {
+  connection: {
     host: 'us-cdbr-iron-east-01.cleardb.net',
     user: 'be50d8aa2153f8',
     password: '0f15e12e',
     database: 'heroku_e5bb1b27e6461d4',
-  },*/
+  },
 });
 
 module.exports = knex;
