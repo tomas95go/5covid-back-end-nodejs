3ec533955475bca7d8114541a699063f8f367661
tomas95go <tomas95go@gmail.com>
2020-04-14 19:00:58 -0300

    refactoreando el codigo

--- api-routes/negocios/negocios.js
+++ api-routes/negocios/negocios.js
@@ -6,6 +6,7 @@ const {
   listadoNegocios,
   listadoFarmacias,
   listadoSuperMercados,
+  listadoNegociosOrdenadoPorRanking,
 } = require('../../controllers/negocios/negocios');
 
 router.post('/listado', (req, res) => {
@@ -17,9 +18,15 @@ router.post('/filtrar/farmacias', (req, res) => {
   const { latitud, longitud } = req.body;
   listadoFarmacias(req, res, latitud, longitud);
 });
+
 router.post('/filtrar/supermercados', (req, res) => {
   const { latitud, longitud } = req.body;
   listadoSuperMercados(req, res, latitud, longitud);
+});
+
+router.post('/filtrar/ranking', (req, res) => {
+  const { latitud, longitud } = req.body;
+  listadoNegociosOrdenadoPorRanking(req, res, latitud, longitud);
 });
 
 module.exports = router;
--- controllers/negocios/negocios.js
+++ controllers/negocios/negocios.js
@@ -24,104 +24,148 @@ const obtenerNegociosSegunTipo = (tipoNegocio, direccionUsuario) => {
   });
 };
 
-const listadoFarmacias = (req, res, latitud, longitud) => {
-  const direccionUsuario = latitud + ',' + longitud;
-  const promiseFarmacias = obtenerNegociosSegunTipo(
-    'pharmacy',
-    direccionUsuario
-  );
-  return promiseFarmacias
-    .then((r) => {
-      const { results } = r.data;
+const promesaFarmacias = (direccionUsuario) => {
+  //obtFarmacias = obtenerFarmacias
+  const obtFarmacias = obtenerNegociosSegunTipo('pharmacy', direccionUsuario);
+  //resPromFarmacia = resolverPromesaFarmacia;
+  const resPromFarmacia = obtFarmacias
+    .then((respuesta) => {
+      //results: es el array de la Google Maps API que contiene los datos.
+      const { results } = respuesta.data;
       const farmacias = results.map((value, i) => {
-        let { name, rating, vicinity } = value;
+        let { name, rating, vicinity, id } = value;
         rating === undefined ? (rating = 'No disponible') : rating;
         const obj = {
+          idFarmacia: id,
           name: name,
           rating: rating,
           vicinity: vicinity,
         };
         return obj;
       });
-      res.status(200).send(farmacias);
       return farmacias;
     })
-    .catch((e) => {
-      console.log(e);
+    .catch((error) => {
+      console.log(error);
+    });
+  return resPromFarmacia;
+};
+
+const promesaSuperMercados = (direccionUsuario) => {
+  //obtSupMer = obtenerSuperMercados
+  const obtSupMer = obtenerNegociosSegunTipo('supermarket', direccionUsuario);
+  //resPromesaSuperMercados;
+  const resPromSupMer = obtSupMer
+    .then((respuesta) => {
+      const { results } = respuesta.data;
+      const superMercados = results.map((value, i) => {
+        let { name, rating, vicinity, id } = value;
+        rating === undefined ? (rating = 'No disponible') : rating;
+        const obj = {
+          idSupermercado: id,
+          name: name,
+          rating: rating,
+          vicinity: vicinity,
+        };
+        return obj;
+      });
+      return superMercados;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  return resPromSupMer;
+};
+
+const listadoFarmacias = (req, res, latitud, longitud) => {
+  const direccionUsuario = latitud + ',' + longitud;
+  //obtenerListaFarmacias
+  const obtListaFarmacias = promesaFarmacias(direccionUsuario);
+  obtListaFarmacias
+    .then((listaFarmacias) => {
+      res.status(200).send(listaFarmacias);
+    })
+    .catch((error) => {
+      console.log(error);
     });
 };
 
 const listadoSuperMercados = (req, res, latitud, longitud) => {
   const direccionUsuario = latitud + ',' + longitud;
-  const promiseSuperMercados = obtenerNegociosSegunTipo(
-    'supermarket',
-    direccionUsuario
-  );
-  return promiseSuperMercados
-    .then((r) => {
-      const { results } = r.data;
-      const superMercados = results.map((value, i) => {
-        let { name, rating, vicinity } = value;
-        rating === undefined ? (rating = 'No disponible') : rating;
-        const obj = {
-          name: name,
-          rating: rating,
-          vicinity: vicinity,
-        };
-        return obj;
-      });
-      res.status(200).send(superMercados);
-      return superMercados;
+  //obtListaSupMer = obtenerListaSuperMercados
+  const obtListaSupMer = promesaSuperMercados(direccionUsuario);
+  obtListaSupMer
+    .then((listaSuperMercados) => {
+      res.status(200).send(listaSuperMercados);
     })
-    .catch((e) => {
-      console.log(e);
+    .catch((error) => {
+      console.log(error);
     });
+};
+
+const combinarListados = (direccionUsuario) => {
+  const obtListaSupMer = promesaSuperMercados(direccionUsuario);
+  const obtListaFarmacias = promesaFarmacias(direccionUsuario);
+  const listaSuperMercados = obtListaSupMer
+    .then((listadoSuperMercados) => {
+      return listadoSuperMercados;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  const listaFarmacias = obtListaFarmacias
+    .then((listadoFarmacias) => {
+      return listadoFarmacias;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  const listadoCompleto = Promise.all([listaSuperMercados, listaFarmacias]);
+  return listadoCompleto;
 };
 
 const listadoNegocios = (req, res, latitud, longitud) => {
   const direccionUsuario = latitud + ',' + longitud;
-  const promiseFarmacias = obtenerNegociosSegunTipo(
-    'pharmacy',
-    direccionUsuario
-  );
-  const promiseSuperMercados = obtenerNegociosSegunTipo(
-    'supermarket',
-    direccionUsuario
-  );
-  promiseFarmacias.then((respuesta) => {
-    const { results } = respuesta.data;
-    const farmacias = results;
-    const listadoFarmacias = farmacias.map((value, i) => {
-      let { id, name, vicinity, rating } = value;
-      rating === undefined ? (rating = 'No disponible') : rating;
-      let farmaciasObj = {
-        idFarmacia: id,
-        nombre: name,
-        direccion: vicinity,
-        rating: rating,
-      };
-      return farmaciasObj;
+  const listaNegocios = combinarListados(direccionUsuario);
+  let listaTotalDeNegocios = [];
+  listaNegocios
+    .then((listadoCompleto) => {
+      let longitudListado = listadoCompleto.length;
+      let i = 0;
+      let comparador = longitudListado - 1;
+      //Mientras que i sea menos al comparador, seguir concatenando los arrays.
+      do {
+        listaTotalDeNegocios = listadoCompleto[i].concat(
+          listadoCompleto[i + 1]
+        );
+        i++;
+      } while (i < comparador);
+      res.status(200).send(listaTotalDeNegocios);
+    })
+    .catch((error) => {
+      console.log(error);
     });
-    promiseSuperMercados.then((respuesta) => {
-      const { results } = respuesta.data;
-      const supermercados = results;
-      const listadoSuperMercados = supermercados.map((value, i) => {
-        let { id, name, vicinity, rating } = value;
-        rating === undefined ? (rating = 'No disponible') : rating;
-        let supermercadosObj = {
-          idSupermercado: id,
-          nombre: name,
-          direccion: vicinity,
-          rating: rating,
-        };
-        return supermercadosObj;
-      });
-      const listadoNegociosCompleto = listadoFarmacias.concat(
-        listadoSuperMercados
-      );
-      res.status(200).send(listadoNegociosCompleto);
-      return listadoNegociosCompleto;
+};
+
+const listadoNegociosOrdenadoPorRanking = (req, res, latitud, longitud) => {
+  const promiseListadoNegocios = listadoNegocios(req, res, latitud, longitud);
+  promiseListadoNegocios.then((data) => {
+    const listadoNegociosOrdenadoPorRanking = data.sort(function (a, b) {
+      if (a.rating === 'No disponible') {
+        a.rating = 0;
+      }
+      if (b.rating === 'No disponible') {
+        b.rating = 0;
+      }
+      if (a.rating > b.rating) {
+        return 1;
+      }
+      if (a.rating < b.rating) {
+        return -1;
+      }
+      return 0;
     });
+    res.status(200).send(listadoNegociosOrdenadoPorRanking.reverse());
   });
 };
 
@@ -129,4 +173,5 @@ module.exports = {
   listadoNegocios: listadoNegocios,
   listadoFarmacias: listadoFarmacias,
   listadoSuperMercados: listadoSuperMercados,
+  listadoNegociosOrdenadoPorRanking: listadoNegociosOrdenadoPorRanking,
 };
